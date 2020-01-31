200808bd27140531d3f2d59fcfd7ee7003275275
Martijn <Martijn9873@hotmail.com>
2020-01-31 15:40:00 +0100

    Added some basic links in top nav

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -32,6 +32,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+						<li><a href="#">Baragenda</a></li>
+						<li><a href="#">Diensten</a></li>
+						<li><a href="#">Management</a></li>
 
                     </ul>
 
@@ -49,7 +52,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a href="{{ route('user.profile') }}">Profiel</a>
+									<a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
