f8be7116e6dd3a2d8229c4e9621a453a3167739b
Kipjr <martijn9873@Hotmail.com>
2020-09-05 11:21:38 +0200

    styling of calendar / create on agenda page

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -27,31 +27,33 @@
 							</nav>
 						</div>
 						<div class="border-div ag-topleft">
-							<a href={{ route('agenda.edit')}}>
-								<div class="ag-create-container">
-									<button class="ag-create-button"  >
-										<span class="ag-create-span"></span>
-										<div class="ag-create-shape"></div>
-										<span class="ag-create-shape2">
-											<div class="ag-create-shape3">
-												<svg width="36" height="36" viewBox="0 0 36 36">
-													<path fill="#34A853" d="M16 16v14h4V20z"></path>
-													<path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
-													<path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
-													<path fill="#EA4335" d="M20 16V6h-4v14z"></path>
-													<path fill="none" d="M0 0h36v36H0z"></path>
-												</svg>
-											</div>
-											<div class="ag-create-text"  >Nieuw</div>
-										</span>
-									</button>
-								</div>
-							</a>
 
 						</div>
 
 						<div class="border-div ag-full" style="">
 							<div class="border-div ag-left" style="">
+                                <div class="ag-create-wrapper">
+                                    <a href={{ route('agenda.edit')}}>
+                                        <div class="ag-create-container">
+                                            <button class="ag-create-button"  >
+                                                <span class="ag-create-span"></span>
+                                                <div class="ag-create-shape"></div>
+                                                <span class="ag-create-shape2">
+                                                    <div class="ag-create-shape3">
+                                                        <svg width="36" height="36" viewBox="0 0 36 36">
+                                                            <path fill="#34A853" d="M16 16v14h4V20z"></path>
+                                                            <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+                                                            <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+                                                            <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
+                                                            <path fill="none" d="M0 0h36v36H0z"></path>
+                                                        </svg>
+                                                    </div>
+                                                    <div class="ag-create-text"  >Nieuw</div>
+                                                </span>
+                                            </button>
+                                        </div>
+                                    </a>
+                                </div>
 								<div class="cal_left">
 									<div class="row">
 										<div class="col-12">
@@ -78,7 +80,8 @@
 
 														$("#datetimepicker13").on("change.datetimepicker", function (e) {
 															iso=moment(e.date._d).toISOString();
-															console.log(iso)
+                                                            console.log(iso)
+                                                           //  window.location.href = "{{ route('agenda.getdate', ['date' => '20200831']) }}" //if we omit JS as primary source
 															try {
 																$.post("{{ route('agenda.getdate') }}", {
 																	date: iso,
